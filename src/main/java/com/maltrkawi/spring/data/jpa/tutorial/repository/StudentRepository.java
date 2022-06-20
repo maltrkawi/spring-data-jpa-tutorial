b102d81cc6beb6ba86500a66dc8c1e4e8b0c0e7f
Mohammad Altrkawi <m.altrkawi@gmail.com>
2022-06-21 00:30:45 +0300

    Initial Commit

---
+++ src/main/java/com/maltrkawi/spring/data/jpa/tutorial/repository/StudentRepository.java
@@ -0,0 +1,31 @@
+package com.maltrkawi.spring.data.jpa.tutorial.repository;
+
+import com.maltrkawi.spring.data.jpa.tutorial.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    List<Student> findByFirstName(String firstName);
+    List<Student> findByFirstNameContains(String name);
+    List<Student> findByLastNameNotNull();
+    List<Student> findByGuardianName(String guardianName);
+    Student findByFirstNameAndLastName(String firstName, String lastName);
+
+    // JPQL
+    @Query("SELECT s FROM Student s WHERE s.emailId = ?1")
+    Student getStudentByEmailAddress(String emailId);
+
+    @Query("SELECT s.firstName FROM Student s WHERE s.emailId = ?1")
+    String getStudentFirstNameByEmailAddress(String emailId);
+
+    @Query(
+            nativeQuery = true,
+            value = "SELECT * FROM student WHERE email_address = ?1"
+    )
+    Student getStudentByEmailAddressNative(String emailId);
+}
---
+++ src/test/java/com/maltrkawi/spring/data/jpa/tutorial/repository/StudentRepositoryTest.java
@@ -0,0 +1,87 @@
+package com.maltrkawi.spring.data.jpa.tutorial.repository;
+
+import com.maltrkawi.spring.data.jpa.tutorial.entity.Guardian;
+import com.maltrkawi.spring.data.jpa.tutorial.entity.Student;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+class StudentRepositoryTest {
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Test
+    public void saveStudent(){
+        Student student = Student.builder()
+                .emailId("f.altrkawi@gmail.com")
+                .firstName("Fares")
+                .lastName("Altrkawi")
+                .build();
+
+        studentRepository.save(student);
+    }
+
+    @Test
+    public void saveStudentGuardian(){
+
+        Guardian guardian = Guardian.builder()
+                .name("Mohammad Altrkawi")
+                .email("m.altrkawi@gmail.com")
+                .mobile("966564111652")
+                .build();
+
+        Student student = Student.builder()
+                .firstName("Julia")
+                .lastName("Altrkawi")
+                .emailId("j.altrkawi@gmail.com")
+                .guardian(guardian)
+                .build();
+
+        studentRepository.save(student);
+    }
+
+    @Test
+    public void printAllStudents(){
+        List<Student> studentList = studentRepository.findAll();
+        System.out.println("studentList" + studentList);
+    }
+
+    @Test
+    public void printStudentByFirstName(){
+        List<Student> studentList = studentRepository.findByFirstName("Julia");
+        System.out.println("studentList" + studentList);
+    }
+
+    @Test
+    public void printStudentByFirstNameContains(){
+        List<Student> studentList = studentRepository.findByFirstNameContains("Ju");
+        System.out.println("studentList" + studentList);
+    }
+
+    @Test
+    public void printStudentBasedOnGuardianName(){
+        List<Student> studentList = studentRepository.findByGuardianName("Mohammad");
+        System.out.println("studentList" + studentList);
+    }
+
+    @Test
+    public void printStudentByEmailAddress(){
+        Student student = studentRepository.getStudentByEmailAddress("f.altrkawi@gmail.com");
+        System.out.println("student: " + student);
+    }
+
+    @Test
+    public void printStudentFirstNameByEmailAddress(){
+        String firstName = studentRepository.getStudentFirstNameByEmailAddress("f.altrkawi@gmail.com");
+        System.out.println("student first name: " + firstName);
+    }
+
+    @Test
+    public void printStudentByEmailAddressNative(){
+        Student student = studentRepository.getStudentByEmailAddressNative("j.altrkawi@gmail.com");
+        System.out.println("student: " + student);
+    }
+}
